0ea2d57427e93f408747212f91d354efa45dbb04
uettechat <uettechatfertyuiop@gmail.Com>
2021-08-21 17:18:00 +0200

    Adding the icon

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+version = (0, 1, 0)  # major, minor, patch
+print(f"Launching FalconEngine v{version[0]}.{version[1]}.{version[2]}...")
+
 import os
 import sys
 import platform
@@ -7,9 +10,4 @@ import pygame
 
 pygame.init()
 
-
-version = (0, 1, 0)  # major, minor, patch
-
-print(f"Launching FalconEngine v{version[0]}.{version[1]}.{version[2]}")
-print(
-    f"Running Uettengine | python {platform.python_version()}, pygame {pygame.version.ver}, SDL {pygame.version.SDL} on {platform.system()} {platform.version()}")
+print(f"Using Python {platform.python_version()}, pygame {pygame.version.ver}, SDL {pygame.version.SDL}, {platform.system()} {platform.version()}")
